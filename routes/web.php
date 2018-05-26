dcab93cdb8bb7736f74bd322a5cac16431ee185f
tinybot <mike@tinybot.ca>
2018-05-26 16:55:19 -0700

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/comments/create', 'HomeController@create');
+Route::post('/comments', 'HomeController@store');
+Route::get('/comments/{comment}', 'HomeController@show');
+Route::get('/comments/{comment}/edit', 'HomeController@edit');
+Route::patch('/comments/{comment}', 'HomeController@update');
+Route::get('/comments/{comment}/delete', 'HomeController@delete');
+Route::delete('/comments/{comment}/delete', 'HomeController@destroy');
