affb45c5a0053be0fd9ce8130e99076ff2a19c14
tinybot <mike@tinybot.ca>
2018-05-26 16:57:25 -0700

    Update CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/public.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
